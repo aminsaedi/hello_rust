37d522277f832e5193b1446011f52d5bd4bb7af7
Amin Saedi <amin.di.sam@gmail.com>
2023-05-12 22:26:06 -0400

    Finshed chapter2: Guess a number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Amin Saedi");
-}
+    println!("Welcome to the \"Guess the number!\"");
+
+    loop { 
+        println!("Please enter your guess.");
+
+        let mut guess = String::new();
+
+        let mut generator = rand::thread_rng();
+
+        let secret_number = generator.gen_range(1..=10);
+
+        println!("Machine generated number is: {}", secret_number);
+
+        let count = io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break;
+            },
+        }
+    }
+} 
